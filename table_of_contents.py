e910c96902f36137475eb31970262ce502486dd7
cub525 <49500036+cub525@users.noreply.github.com>
2020-11-18 17:24:18 -0700

    Update table_of_contents.py

--- table_of_contents.py
+++ table_of_contents.py
@@ -7,15 +7,15 @@ Created on Mon Nov  9 12:56:30 2020
 
 import re
 
-f = open('family-songbook/songbook.html')
 contents_dict = {}
 label_dict = {'1':'chapterref','2':'songref','3':''}
 
-for line in f:
-    a = re.match('\s*<\s*h([1|2])\s*id="(.*?)">',line)
-    if a != None:
-        b = re.findall('>\s*([^<]+?)\s*<', line)
-        print('<a class = "{0}" href = #{1}>{2}</a><br>'.format(
-            label_dict[a.group(1)],
-            a.group(2),
-            b[0]))
+with open('family-songbook/songbook.html','r') as file:
+    for line in file:
+        a = re.match('\s*<\s*h([1|2])\s*id="(.*?)">',line)
+        if a:
+            b = re.findall('>\s*([^<]+?)\s*<', line)
+            print('<a class = "{0}" href = #{1}>{2}</a><br>'.format(
+                label_dict[a.group(1)],
+                a.group(2),
+                b[0]))
