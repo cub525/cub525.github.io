1762afc0304363bcd3a5d846369adc5fda7b24a5
cub525 <49500036+cub525@users.noreply.github.com>
2021-04-14 20:23:09 -0700

    Implement relative units for text.

--- family-songbook/table-of-contents.js
+++
@@ -1,4 +0,0 @@
-templateContent = document.querySelector('template').content;
-article = document.querySelector('article').cloneNode(true);
-article.attachShadow({  mode: 'closed' }).appendChild(templateContent.cloneNode(true));
-document.querySelector('#toc').appendChild(article);
